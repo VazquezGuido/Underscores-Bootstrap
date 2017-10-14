66c80698437cb131f3707fd80fab5e2930d08dce
deeve007 <dave69@gmail.com>
2017-10-14 03:42:38 -0300

    Loads of updates

--- underscores-bootstrap/functions.php
+++ underscores-bootstrap/functions.php
@@ -168,6 +168,7 @@ remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
 remove_action( 'wp_print_styles', 'print_emoji_styles' );
 remove_action( 'admin_print_styles', 'print_emoji_styles' );
 
+
 /**
  * Adding Bootstrap elements to Wordpress comments form
  */
