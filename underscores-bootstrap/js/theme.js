ef8693112ae89205b57767cdfa0be1add5c5a0f7
deeve007 <dave69@gmail.com>
2017-10-19 15:00:30 -0300

    New file for custom theme JS

---
+++ underscores-bootstrap/js/theme.js
@@ -0,0 +1,12 @@
+/**
+ * File theme.js.
+ *
+ * General JS functions for theme
+ */
+
+( function( $ ) {
+
+	// Call Mmenu function
+	$('#site-navigation').mmenu({}, {clone: true});
+	
+} )( jQuery );
