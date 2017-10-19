78dfbae774da1163178ed808cb94f98f94e1bd2e
deeve007 <dave69@gmail.com>
2017-10-19 14:59:43 -0300

    Added theme.js call

--- underscores-bootstrap/functions.php
+++ underscores-bootstrap/functions.php
@@ -111,6 +111,7 @@ function underscores_bootstrap_scripts() {
 
 	wp_enqueue_script( 'underscores-bootstrap-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
 	wp_enqueue_script( 'mmenu-js', get_template_directory_uri() .'/js/jquery.mmenu.js', array('jquery'), null, true);
+	wp_enqueue_script( 'theme-js', get_template_directory_uri() .'/js/theme.js', array('jquery','mmenu-js'), null, true);
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
